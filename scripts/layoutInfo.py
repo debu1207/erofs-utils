6d7ad252bcb961c6dd3a808498137f21bdbb282c
debu1207 <patradebu1207@gmail.com>
2026-05-24 11:11:18 +0530

    Add description in script/layoutInfo.py helper script

--- scripts/layoutInfo.py
+++ scripts/layoutInfo.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Helper python script to show the compressed block information in easy to understand and debug format
+Usage: 
+    > ./fsck.erofs --blocks-layout=blocksinfo image.erofs
+    > python3 ./scripts/layoutInfo.py blocksinfo
+"""
 
 import sys
 
